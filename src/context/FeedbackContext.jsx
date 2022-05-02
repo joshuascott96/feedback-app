170e2ffc62f6eb5f11f444eabcd1b68fad9ea0c6
Joshua Scott <joshuascott96@live.com>
2022-05-02 13:13:14 +1000

    added backend and loader

--- src/context/FeedbackContext.jsx
+++ src/context/FeedbackContext.jsx
@@ -1,20 +1,30 @@
 import { v4 as uuidv4 } from 'uuid'
-import { createContext, useState } from 'react'
+import { createContext, useState, useEffect } from 'react'
 
 const FeedbackContext = createContext()
 
 export const FeedbackProvider = ({children}) => {
-    const [feedback, setFeedback] = useState([
-        {
-            id:1,
-            text: 'this item is context',
-            rating: 10,
-        }
-    ])
+    const [isLoading, setIsLoading] = useState(true)
+    const [feedback, setFeedback] = useState([])
     const [feedbackEdit, setFeedbackEdit] = useState({
         item: {},
         edit: false,
     })
+
+    useEffect(() => {
+        fetchFeedback()
+    }, [])
+
+    // fetch feedback 
+    const fetchFeedback = async () => {
+        const response = await fetch("http://localhost:4000/feedback?_sort=id&order=desc")
+        const data = await response.json()
+
+        setFeedback(data)
+        setIsLoading(false)
+    }
+
+
     // Add Feedback
     const addFeedback = (newFeedback) => {
         newFeedback.id = uuidv4()
@@ -50,6 +60,7 @@ export const FeedbackProvider = ({children}) => {
     return <FeedbackContext.Provider value={{
         feedback,
         deleteFeedback,
+        isLoading,
         addFeedback,
         editFeedback,
         feedbackEdit,

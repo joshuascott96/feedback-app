6bbe9a629e55a465caee004f930acadbd6504207
Joshua Scott <joshuascott96@live.com>
2022-05-04 11:32:56 +1000

    finalised edit and delete to backend

--- src/context/FeedbackContext.jsx
+++ src/context/FeedbackContext.jsx
@@ -1,4 +1,4 @@
-import { v4 as uuidv4 } from 'uuid'
+
 import { createContext, useState, useEffect } from 'react'
 
 const FeedbackContext = createContext()
@@ -17,7 +17,7 @@ export const FeedbackProvider = ({children}) => {
 
     // fetch feedback 
     const fetchFeedback = async () => {
-        const response = await fetch("http://localhost:4000/feedback?_sort=id&order=desc")
+        const response = await fetch("/feedback?_sort=id&order=desc")
         const data = await response.json()
 
         setFeedback(data)
@@ -26,21 +26,41 @@ export const FeedbackProvider = ({children}) => {
 
 
     // Add Feedback
-    const addFeedback = (newFeedback) => {
-        newFeedback.id = uuidv4()
-        setFeedback([newFeedback, ...feedback])
+    const addFeedback = async (newFeedback) => {
+        const response = await fetch('/feedback', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(newFeedback),
+        })
+
+        const data = await response.json()
+        setFeedback([data, ...feedback])
     }
 
     // Delete Feedback
-    const deleteFeedback = (id) => {
+    const deleteFeedback = async (id) => {
         if (window.confirm('Are you sure you want to delete?')) {
+            await fetch(`/feedback/${id}`, {method: 'DELETE'})
+
             setFeedback(feedback.filter((item) => item.id !== id))
         }
     }
 
     // Update Feedback
-    const updateFeedback = (id, updItem) => {
-        setFeedback(feedback.map((item) => (item.id === id ? {...item, ...updItem } : item ))
+    const updateFeedback = async (id, updItem) => {
+        const response = await fetch(`/feedback/${id}`, {
+            method: 'PUT',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(updItem)
+        })
+
+        const data = await response.json()
+
+        setFeedback(feedback.map((item) => (item.id === id ? {...item, ...data } : item ))
         )
 
         setFeedbackEdit({
